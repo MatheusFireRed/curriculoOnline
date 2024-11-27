f49af1b708e4d49be6270fe85a313ec7d78f7ce6
Matheus de Oliveira Timotheo <111666891+MatheusFireRed@users.noreply.github.com>
2024-11-27 10:27:09 -0300

    Início construção de páginas secundárias
    Dado início da formulação das páginas de certificados e projetos. Página de certificados, iniciando a construção dos menus dropdown para as linguagens de programação. Página de projetos, apenas estrutura básica com o header.

---
+++ js/fadeInTexto.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+
+    const p1    = document.getElementById('texto-2');
+    const p2    = document.getElementById('texto-1');
+
+    setTimeout(() => {
+
+        p1.style.opacity = 1;
+        p2.style.opacity = 1;
+    }, 100);
+}
