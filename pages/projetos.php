532b768ada9594bd0bbc0756bd515ab7bb81e507
Matheus de Oliveira Timotheo <111666891+MatheusFireRed@users.noreply.github.com>
2026-03-07 00:46:42 -0300

    Página projetos - Filtro
    Foi feita a estilização dos checkboxs do filtro.

--- pages/projetos.php
+++ pages/projetos.php
@@ -30,30 +30,101 @@
             <button id="btnFiltro"><img src="http://localhost/paginapessoal/img/caret-down.svg" alt="Seta para abaixar o filtro." width="40px"></button>
         </div>
 
-        <div class="filtro">
-            <label>
-                <input type="checkbox" value="python"><img src="../img/icons/icons8-python-48.png" alt="Filtrar para python.">
-            </label>
-            
-            <label>
-                <input type="checkbox" value="html"> <img src="../img/icons/icons8-html-48.png" alt="Filtrar para HTML">
+        <!-- PASSAR PARA A PÁGINA DE STYLE !-->
+        <style>
+            .img-linguagens{
+                width: 45px;
+                height: 45px;
+            }
+
+            .cb-linguagens{
+                background: none;
+                outline: none;
+            }
+
+            .checkbox-container{
+                display: flex;
+                align-items: center;
+                cursor: pointer;
+                font-family: Arial;
+                gap: 10px;
+                padding: 5px;
+            }
+
+            .checkbox-container input{
+                display: none;
+            }
+
+            .checkmark{
+                border-radius: 20px;
+                transition: ease-in-out .5s;
+                display: flex;
+                align-items: center;
+                padding: 3px;
+            }
+
+            .checkbox-container input:checked + .checkmark{
+                background-color: #c0bdb3;
+                box-shadow: 1px 1px 5px 3px white;
+            }
+
+        </style>
+
+        <div class="checkbox-container">
+            <label class="checkbox-container">
+                <input type="checkbox" value="python">
+                <span class="checkmark">
+                    <img src="../img/icons/icons8-python-48.png" class="img-linguagens" alt="Filtrar para python.">
+                </span>
             </label>
 
-            <label>
-                <input type="checkbox" value="css"> <img src="../img/icons/icons8-css-48.png" alt="Filtrar para CSS.">
+            <label class="checkbox-container">
+                <input type="checkbox" value="html">
+                <span class="checkmark">
+                    <img src="../img/icons/icons8-html-48.png" class="img-linguagens" alt="Filtrar para HTML.">
+                </span>
             </label>
 
-            <label>
-                <input type="checkbox" value="javascript"><img src="../img/icons/icons8-javascript-48.png" alt="Filtrar para JavaScript.">
-
-            <label>
-                <input type="checkbox" value="sql"> <img src="../img/icons/icons8-sql-48.png" alt="Filtrar para SQL.">
+            <label class="checkbox-container">
+                <input type="checkbox" value="css">
+                <span class="checkmark">
+                    <img src="../img/icons/icons8-css-48.png" class="img-linguagens" alt="Filtrar para CSS.">
+                </span>
             </label>
+
+            <label class="checkbox-container">
+                <input type="checkbox" value="php">
+                <span class="checkmark">
+                    <img src="../img/icons/php.png" class="img-linguagens" alt="Filtrar para PHP.">
+                </span>
+            </label>
+    
+            <label class="checkbox-container">
+                <input type="checkbox" value="javascript">
+                <span class="checkmark">
+                    <img src="../img/icons/icons8-javascript-48.png" class="img-linguagens" alt="Filtrar para JavaScript.">
+                </span>
+            </label>
+
+            <label class="checkbox-container">
+                <input type="checkbox" value="sql">
+                <span class="checkmark">
+                    <img src="../img/icons/icons8-sql-48.png" class="img-linguagens" alt="Filtrar para SQL.">
+                </span>
+            </label>
+
+            <label class="checkbox-container">
+                <input type="checkbox" value="java">
+                <span class="checkmark">
+                    <img src="../img/icons/java.png" class="img-linguagens" alt="Filtrar para Java.">
+                </span>
+            </label>
+
         </div>
 
         <div class="box-projetos">
             
-            <!-- PASSAR PARA A PÁGINA DE STYLE !-->
+
             <style>
             .card-projeto{
                 border: 1px solid green;
@@ -63,6 +134,8 @@
                 flex-direction: column;
                 align-items: center;
                 padding: 20px 0;
+                background-color: #c0bdb3;
+                border-radius: 20px;
             }
             .img-projeto{
                 width: 90%;
